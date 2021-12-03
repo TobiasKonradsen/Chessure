7edebfeb1785cdac174c4e55995e6e5f78510074
unknown <TobiasKonradsen@outlook.com>
2021-12-04 00:37:16 +0100

    Team class

--- game_logic/team.py
+++ game_logic/team.py
@@ -1,4 +1,15 @@
 
 
-class Team:
-    pass
+class Team(int):
+    def __new__(cls, value, *args, **kwargs):
+        return int.__new__(cls, value)
+    
+        
+class White(Team):
+    def __init__(self):
+        super().Team.__init__(1)
+
+    
+class Black(Team):
+    def __init__(self):
+        super().Team.__init__(-1)
